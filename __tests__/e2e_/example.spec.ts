87d04f94291b6c9db7e45729118a976f9e0bf8d0
Angel Marquez <angel.marquez@gmail.com>
2024-11-07 05:59:41 -0700

    updated playwright example

--- __tests__/e2e_/example.spec.ts
+++ __tests__/e2e_/example.spec.ts
@@ -4,9 +4,9 @@ test("should navigate to the about page", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("http://localhost:3000/")
   // Find an element with the text 'About' and click on it
-  await page.click("text=About")
+  await page.click("text=Sunburst")
   // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL("http://localhost:3000/about")
+  await expect(page).toHaveURL("http://localhost:3000/sunburst")
   // The new page should contain an h1 with "About"
-  await expect(page.locator("h1")).toContainText("About")
+  await expect(page.locator("h1")).toContainText("Sunburst")
 })
